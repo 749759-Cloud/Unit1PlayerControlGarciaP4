c167a050e5d55e09f9eda88d2e91046a2b2774f1
749759-Cloud <749759@student.dusd.net>
2026-02-10 11:37:41 -0800

    obstacles made

--- P4PlayerControlGarcia/Assets/Scripts/PlayerController.cs
+++ P4PlayerControlGarcia/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
+    private float speed = 15.0f;
+    private float turnSpeed = 45.0f;
     private float horizontalInput;
-    public float forwardInput;
+    private float forwardInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,11 +18,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // This is where we get player input
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
         // We'll move the vehicle forward 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // We turn the vehicle
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
